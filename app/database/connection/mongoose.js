d1a2de42d7d9018a9c2d701cdfb7d5296f6395c3
Oleh Paryshkura <Oleh_Paryshkura@epam.com>
2020-03-16 06:30:41 +0200

    delete unused import

--- app/database/connection/mongoose.js
+++ app/database/connection/mongoose.js
@@ -1,3 +1,2 @@
 let mongoose = require('mongoose');
-let mongoConfig = require('../../../config/mongo');
 mongoose.connect(`mongodb+srv://oleh_paryshkura:oleg911119@cluster0-ettw7.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true });
